7e4371556064d9b80fba5683d3e358f924def8b2
Aye Chan Labs <acn.alexchan@gmail.com>
2026-04-19 15:43:52 +0300

    day44: dom object model

---
+++ notes/javascript/l52documentobjectmodel.js
@@ -0,0 +1,20 @@
+console.log("Hello World!");
+
+// DOM (Document Object Model), programming interface 
+// for HTML and XML documents. It represents the page so that 
+// programs can change the document structure, style, and content. 
+// The DOM represents the document as nodes and objects; that way, 
+// programming languages can interact with the page.
+
+// to access Web Page (HTML Document)
+// i. search
+// ii. create new element
+// iii. edit
+// iv. remove
+// v. action to Events
+
+// getElementById() => ID => #
+// getElementsByClassName() => Class => .
+// getElementsByTagName() => Tag => tag name
+// querySelector() => ID, Class, Tag အားလုံးကို ခေါ် လို့ ရနေပြီ။ သူမှာ class, id, tag စသဖြင့် ကို CSS Selector လိုမျိုး ခေါ် ဖို့ လိုတယ်။
+// querySelectorAll() => သူက တော့ querySelector ကို အားလုံးကို ခေါ် လို့ ရနေပြီ။ သူမှာ class, id, tag စသဖြင့် ကို CSS Selector လိုမျိုး ခေါ် လို့ ရတယ် ဒါမဲ့ သူက array နဲ့ လာမှာ။
