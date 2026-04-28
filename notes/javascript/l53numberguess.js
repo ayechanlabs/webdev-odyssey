48f451999e131977334d8be689b149cd6a05d27d
Aye Chan Labs <acn.alexchan@gmail.com>
2026-04-28 13:33:19 +0300

    day53: finish number guess game

--- notes/javascript/l53numberguess.js
+++ notes/javascript/l53numberguess.js
@@ -9,7 +9,7 @@ const min_number = document.querySelector(".min_number"),
 
 const min_num = 1,
     max_num = 10,
-    win_num = 5;
+    win_num = randomNum(min_num, max_num);
 
 let game_left = 3;
 
@@ -30,35 +30,44 @@ get_btn.addEventListener("click", function(e) {
     if (guess === win_num) {
         // GAME WON
 
-        get_input.disabled = true;
+        // // disabled the input box
+        // get_input.disabled = true;
 
-        get_input.style.borderColor = "green";
+        // // change border color of input box to green
+        // get_input.style.borderColor = "green";
         
-        // show message alert if user win the game & change text box color to green.
-        // message_1.textContent = `${win_num} is correct. You win the game.`;
-        // message_1.style.color = "green";
-        setMessage_1(`${win_num} is correct. You win the game.`, "green");
+        // // show message alert if user win the game & change text box color to green.
+        // // message_1.textContent = `${win_num} is correct. You win the game.`;
+        // // message_1.style.color = "green";
+        // setMessage_1(`${win_num} is correct. You win the game.`, "green");
 
-        get_btn.value = "Play Again!";
+        // get_btn.value = "Play Again!";
+
+        game_end(true, `${win_num} is correct. You win the game.`);
+
+
     } else {
         game_left -= 1;
 
         if (game_left === 0) {
             // GAME-OVER
 
-            // disabled the input box
-            get_input.disabled = true;
+            // // disabled the input box
+            // get_input.disabled = true;
 
-            // change border color of input box to red
-            get_input.style.borderColor = "red";
+            // // change border color of input box to red
+            // get_input.style.borderColor = "red";
 
-            // show message alert if user lost the game & change text box color to red.
-            // message_1.textContent = `Game Over, You Lose the game. The correct number is ${win_num}.`;
-            // message_1.style.color = "red";
-            setMessage_1(`Game Over, You Lose the game. The correct number is ${win_num}.`, "red");
+            // // show message alert if user lost the game & change text box color to red.
+            // // message_1.textContent = `Game Over, You Lose the game. The correct number is ${win_num}.`;
+            // // message_1.style.color = "red";
+            // setMessage_1(`Game Over, You Lose the game. The correct number is ${win_num}.`, "red");
 
-            // play again
-            get_btn.value = "Play Again!"
+            // // play again
+            // get_btn.value = "Play Again!"
+
+            game_end(false, `Game Over, You Lose the game. The correct number is ${win_num}.`);
+
         } else {
             // CONTINUE GAME
 
@@ -96,4 +105,41 @@ function setMessage_2 (msg, color) {
         message_2.textContent = "";
     }, 2000);
 }
+
+function game_end(won, msg) {
+    // let color = won === true ? "green" : "red";
+
+    let color;
+    won === true ? color = "green" : color = "red";
+
+    get_input.disabled = true;
+    get_input.style.borderColor = color;
+
+    setMessage_1(msg, color);
+
+    get_btn.value = "Play Again";
+
+    // add class
+
+    // get_btn.className = "btn reload";
+    // get_btn.className += " reload";
+
+    get_btn.classList.add("reload");
+}
+
+// click = mouseup
+
+get_game_form.addEventListener ("mousedown", function(e) {
+    if (e.target.classList.contains("reload")) {
+        window.location.reload();
+    }
+})
+
+function randomNum (min, max) {
+    let getRandom = Math.round(Math.random() * (max - min) + 1);
+    return getRandom;
+}
+
+// console.log(randomNum(1, 10));
+console.log(win_num);
 
