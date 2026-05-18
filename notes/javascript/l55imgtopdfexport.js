faeceac90f95846c4bf6cd0a3bd956c9f7b3db80
Aye Chan Labs <acn.alexchan@gmail.com>
2026-05-18 10:31:01 +0300

    day61: final image-to-pdf-export-file

--- notes/javascript/l55imgtopdfexport.js
+++ notes/javascript/l55imgtopdfexport.js
@@ -24,12 +24,58 @@ getfileinput.addEventListener ('change', function(e) {
 });
 
 function pdfloader() {
+    // 1. check if image exists or not
     if (!newimg) {
         window.alert("Please upload an image first!");
         return;
     }
+
+    // 2. import package and initialize
+    const { jsPDF } = window.jspdf;
+
+    // Default export is a4 paper, portrait, using millimeters for units
+    const pdfObj = new jsPDF({
+        orientation: "landscape",
+        unit: "mm"
+    });
+
+    // 3. create canvas as image actual dimensations
+    const canvas = document.createElement('canvas');
+    canvas.width = newimg.width; // width or naturalWidth
+    canvas.height = newimg.height; // height or naturalHeight
+
+    // console.log(canvas);
+
+    // 4. draw image on canvas
+    // drawing context
+    const ctx = canvas.getContext("2d");
+
+    // ctx.drawImage(image, destination-x, destination-y,)
+    ctx.drawImage(newimg, 0, 0);
+
+    // 5. convert canvas to JPEG (Quality 0 to 1)
+    // canvas.toDataURL(img-type, img-quality) -> img-type = image/png, image/webp, image/jpeg
+    const imgdata = canvas.toDataURL("image/jpeg", 0.85);
+
+    // 6. download img to pdf
+    // pdfObj.text("Hello, World!", 10, 10);
+
+    // addImage(image, X-coordinate, Y-coordinate, width, height)
+    pdfObj.addImage(imgdata, 10, 10);
+    pdfObj.save("newImgtoPdfFile.pdf");
 }
 
 // https://www.npmjs.com/package/jspdf
 // https://github.com/parallax/jsPDF
+
+// console.log(window.jspdf); // ဒါက class တွေ အားလုံးကို ခေါ် ထုတ်လိုက်တာ
+// console.log(window.jspdf.jsPDF); // ဒါက တော့ jspdf ထဲမှာ ရှိတဲ့ methods တွေပဲ ခေါ် ထုတ်တာ
+
+// Method 1
+// const jspdfObj = window.jspdf.jsPDF;
+// console.log(jspdfObj);
+
+// // Method 2 (Object Destructuring)
+// const { jsPDF } = window.jspdf;
+// console.log(jsPDF);
     
