8e984d72bf3eef76a9aa355dfe82cee30916f596
Aye Chan Labs <acn.alexchan@gmail.com>
2026-05-27 18:09:19 +0300

    day67: temperature-converter-v1

--- notes/projects/temperature_converter/script.js
+++ notes/projects/temperature_converter/script.js
@@ -1,1 +1,83 @@
-console.log("Temperature Converter App.");
+// get DOM elements from UI
+const inputTemp = document.querySelector("#temp-decimal-input"),
+      fromUnit = document.querySelector("#from-temp-units"), 
+      toUnit = document.querySelector("#to-temp-units"), 
+      convertBtn = document.querySelector("#convert-btn"),
+      resultTxt = document.querySelector(".result-text");
+
+function checkGetInput() {
+    if (
+        (inputTemp.value.trim() !== "") &&
+        (fromUnit.value !== "FU") &&
+        (toUnit.value !== "TU")
+    ) {
+        console.log(false);
+        convertBtn.disabled = false;
+    } else {
+        console.log(true);
+        convertBtn.disabled = true;
+    }
+}
+
+inputTemp.addEventListener ("input", checkGetInput);
+fromUnit.addEventListener ("change", checkGetInput);
+toUnit.addEventListener ("change", checkGetInput);
+
+checkGetInput();
+
+function convertTemp() {
+    let currentTemp = Number(inputTemp.value), convertTemp = 0;
+
+    // Convert Fahrenheit to Celsius
+    if (fromUnit.value === "F" && toUnit.value === "C") {
+        convertTemp = (currentTemp - 32) * 5/9;
+    } 
+    // Convert Fahrenheit to Kelvin
+    else if (fromUnit.value === "F" && toUnit.value === "K") {
+        convertTemp = (currentTemp - 32) * 5/9 + 273.15;
+    }
+    // Convert Fahrenheit to Fahrenheit
+    else if (fromUnit.value === "F" && toUnit.value === "F") {
+        convertTemp = currentTemp;
+    }
+    // Convert Celsius to Fahrenheit
+    else if (fromUnit.value === "C" && toUnit.value === "F") {
+        convertTemp = (currentTemp * (9/5)) + 32;
+    }
+    // Convert Celsius to Kelvin
+    else if (fromUnit.value === "C" && toUnit.value === "K") {
+        convertTemp = (currentTemp + 2733.15);
+    }
+    // Convert Celsius to Celsius
+    else if (fromUnit.value === "C" && toUnit.value === "C") {
+        convertTemp = currentTemp;
+    }
+    // Convert Kelvin to Fahrenheit
+    else if (fromUnit.value === "K" && toUnit.value === "F") {
+        convertTemp = (currentTemp - 273.15) * 9/5 +32;
+    }
+    // Convert Kelvin to Celsius
+    else if (fromUnit.value === "K" && toUnit.value === "C") {
+        convertTemp = currentTemp - 273.15;
+    }
+    // Convert Kelvin to Kelvin
+    else {
+        convertTemp = currentTemp
+    }
+
+    return convertTemp;
+}
+
+convertBtn.addEventListener("click", () => {
+    result = convertTemp();
+   
+    // resultTxt.innerHTML or textContent
+    // The result should be like this -> 34 Celsius is 93.2 Fahrenheit.
+    resultTxt.textContent = `
+    ${inputTemp.value} 
+    ${fromUnit.options[fromUnit.selectedIndex].text} 
+    is ${result}
+    ${toUnit.options[toUnit.selectedIndex].text}.`;
+
+    console.log(result);
+});
